b88e7e1304d5ffc3a66daa861d4dbfefc708c7b8
Yixuan <90607794+namelesskid123@users.noreply.github.com>
2022-09-05 09:38:56 +0800

    Add files via upload

---
+++ RPi/main.py
@@ -0,0 +1,16 @@
+import os
+import argparse
+
+from src.communicator.MultiProcessCommunication import MultiProcessCommunicator
+
+
+def init():
+    #os.system("sudo hciconfig hci0 piscan")
+    multiprocess_communication_process = MultiProcessCommunicator()
+    multiprocess_communication_process.start()
+
+
+
+if __name__ == '__main__':
+    init()
+ 
