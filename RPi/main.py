8fd9b84f089af73e66fbfb8b7f7f365fc5af78ea
song0180 <song0180@e.ntu.edu.sg>
2022-10-12 14:45:09 +0800

    rewrite task2

--- RPi/main.py
+++ RPi/main.py
@@ -7,11 +7,8 @@ from src.communicator.MultiProcessCommunication import MultiProcessCommunicator
 
 def init():
     #os.system("sudo hciconfig hci0 piscan")
-    # multiprocess_communication_process = MultiProcessCommunicator()
-    # multiprocess_communication_process.start()
-    task2_algo = Task2()
-    # note: _allow_reconnection can block the run method!
-    task2_algo.run()
+    multiprocess_communication_process = MultiProcessCommunicator()
+    multiprocess_communication_process.start()
 
 
 
